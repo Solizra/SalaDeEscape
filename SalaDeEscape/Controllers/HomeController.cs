00da30fbe3c2203b6728ca4f85f96f5d4c071bc5
Luckxyor <lucaseialben2@gmail.com>
2024-07-23 20:32:32 -0300

    Add files via upload

---
+++ SalaDeEscape/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SalaDeEscape.Models;
+
+namespace SalaDeEscape.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Tutorial(){
+        return View();
+    }
+
+    public IActionResult Comenzar(){
+        int habitacion=Escape.GetEstadoJuego();
+        return View($"habitacion{habitacion}");
+    }
+
+    public IActionResult Habitacion(int sala, string clave){
+        int habitacion = Escape.GetEstadoJuego();
+        if (sala!=habitacion){
+            return View($"habitacion{habitacion}");
+        }
+        
+        if (Escape.ResolverSala(sala, clave)){
+            if(habitacion<5){
+                return View($"habitacion{habitacion+1}");
+            }
+            else{
+                return View($"habitacion{5}");
+            }
+        }
+        else{
+            ViewBag.MensajeError = "La clave es incorrecta";
+            return View($"habitacion{habitacion}");
+        }
+    }
+
+    public IActionResult Creditos(){
+        return View();
+    }
+}
